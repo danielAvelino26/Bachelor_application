359801a1760bc08ca45fcbb5b147309be5784a87
danielAvelino26 <71258905+danielAvelino26@users.noreply.github.com>
2023-07-11 14:17:27 +0200

    Ajout du build Android dans le dossier build/outputs/flutter-apk

---
+++ build/app/generated/source/buildConfig/release/com/example/crossplateforme/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.crossplateforme;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.crossplateforme";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
